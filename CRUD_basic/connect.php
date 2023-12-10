9b6e643a124c9a44a362fa1eb8f1faf3b83d4dec
Cain <cainmartinez2@hotmail.com>
2023-12-10 23:31:49 +0100

    Update conn with env variables

--- CRUD_basic/connect.php
+++ CRUD_basic/connect.php
@@ -1,6 +1,10 @@
 <?php
+    // Preparación de variables de entorno para la conexión a la base de datos
+    // $db_host = getenv('DB_HOST');
+    // $db_name = getenv('DB_NAME');
     // $db_password = getenv('DB_PASSWORD');
-
+    // $db_user = getenv('DB_USER');
+    // $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
     $conn = new mysqli('cochescain.net', 'root', 'camarber1', 'cars');
 
     if ($conn->connect_error) {
