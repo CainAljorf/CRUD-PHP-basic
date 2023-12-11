9bce025ea02cdeb2724438ce6f15f61672bdb874
Cain <cainmartinez2@hotmail.com>
2023-12-12 00:00:43 +0100

    add env variables to connect

--- CRUD_basic/connect.php
+++ CRUD_basic/connect.php
@@ -1,16 +1,20 @@
+
 <?php
+    // Carga las variables de entorno desde el archivo .ini
+    $env = parse_ini_file('C:/sites/.env.ini');
+
     // Preparación de variables de entorno para la conexión a la base de datos
-    // $db_host = getenv('DB_HOST');
-    // $db_name = getenv('DB_NAME');
-    // $db_password = getenv('DB_PASSWORD');
-    // $db_user = getenv('DB_USER');
-    // $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-    $conn = new mysqli('cochescain.net', 'root', 'camarber1', 'cars');
+    $db_host = $env['DB_HOST'];
+    $db_name = $env['DB_NAME'];
+    $db_password = $env['DB_PASSWORD'];
+    $db_user = $env['DB_USER'];
+
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
     if ($conn->connect_error) {
         die('Error de Conexión (' . $conn->connect_errno . ') '
                 . $conn->connect_error);
-    }else{
+    } else {
         echo 'Conexión correcta a ' . $conn->host_info . "\n";
     }
 ?>
